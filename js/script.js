d296ef649b27611f042324f990d37a133037de90
Animesh Dhakal <animesh2063@gmail.com>
2026-06-27 10:11:40 +0545

    buttons-added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-const startBtn = document.getElementById("startBtn");
+const emergencyButton = document.querySelector(".emergency-btn");
+const emergencyMenu = document.querySelector(".emergency-menu");
 
-startBtn.addEventListener("click", () => {
-    alert("Welcome to Tourist Safety!");
+let open = false;
+
+emergencyButton.onclick = () => {
+
+    open = !open;
+
+    emergencyMenu.style.display = open ? "flex" : "none";
+
+};
+
+// Close popup when clicking outside
+document.addEventListener("click", (e) => {
+
+    if (
+        !emergencyButton.contains(e.target) &&
+        !emergencyMenu.contains(e.target)
+    ) {
+
+        emergencyMenu.style.display = "none";
+        open = false;
+
+    }
+
 });
